3fb54b1b7ab5639579feb7924d7cfab8c6794534
noah <noahmiller@hotmail.ca>
2026-06-04 19:59:04 -0400

    feat: added header

--- resources/js/components/Header.tsx
+++ resources/js/components/Header.tsx
@@ -1,3 +1,59 @@
+import { useState } from 'react';
+import { ListCollapse, User } from 'lucide-react';
+
 export default function Header() {
-    return <></>;
-};
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <header className="border-b bg-neutral-900">
+            <div className="relative mx-auto flex h-16 max-w-7xl items-center justify-between px-6">
+                {/* Left */}
+                <div className="flex flex-1 justify-end pr-10">
+                    <a
+                        href="#"
+                        className="text-lg font-medium text-neutral-400 hover:text-white"
+                    >
+                        Lists
+                    </a>
+                </div>
+
+                {/* Center Logo */}
+                <div className="flex-shrink-0">
+                    <ListCollapse size={35} className = "text-white"/>
+                </div>
+
+                {/* Right */}
+                <div className="flex flex-1 items-center justify-start pl-10">
+                    <a
+                        href="#"
+                        className="text-lg font-medium text-neutral-400 hover:text-white"
+                    >
+                        Tasks
+                    </a>
+                </div>
+
+                {/* User Menu */}
+                <div className="absolute right-6">
+                    <div className="relative">
+                        <button
+                            onClick={() => setMenuOpen(!menuOpen)}
+                            className="rounded-full p-2 hover:bg-neutral-100"
+                        >
+                            <User className="text-neutral-400"/>
+                        </button>
+
+                        {menuOpen && (
+                            <div className="absolute right-0 mt-3 w-40 border bg-neutral-900">
+                                <button
+                                    className="block w-full px-4 py-3 text-neutral-400 font-medium text-left text-sm hover:bg-neutral-100"
+                                >
+                                    Logout
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
---
+++ resources/js/pages/Login.tsx
@@ -0,0 +1,21 @@
+import { Head, Link, usePage } from '@inertiajs/react';
+
+export default function Home() {
+    const { auth } = usePage().props;
+
+    return (
+        <>
+            <Head title="Login"></Head>
+
+            {auth.user ? (
+                <>
+                    Redirect to homepage
+                </>
+            ) : (
+                <>
+                    Show the login form
+                </>
+            )}
+        </>
+    );
+}
